fdedd465ae7efac918e3ac7c5f5e4586bc8cd3fe
Igor Santos <igors.d@hotmail.com>
2020-04-10 17:13:48 -0300

    ajustes nas colunas da migration de produto

---
+++ database/migrations/2020_04_10_165424_create_product_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Sku');
+            $table->string('name');
+            $table->longText('description');
+			$table->decimal('price');
+            $table->integer('amount');
+			$table->boolean('active')->default(1);
+            $table->boolean('Insert_in');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
