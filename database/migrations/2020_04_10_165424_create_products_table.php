58c34f3720bc6a3048116d0a8b41bdf13efc850b
Igor Santos <igors.d@hotmail.com>
2020-04-11 00:42:49 -0300

    ajustes na migration de produtos

---
+++ database/migrations/2020_04_10_165424_create_products_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sku')->unique();
+            $table->string('name');
+            $table->longText('description');
+			$table->decimal('price');
+            $table->integer('amount');
+			$table->boolean('active')->default(1);
+            $table->boolean('insert_in');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->string('name');
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('product_images');
+    }
+}
