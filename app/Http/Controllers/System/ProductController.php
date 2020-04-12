b342ca553779e36e1b6925fade63178978a400ef
Igor Santos <igors.d@hotmail.com>
2020-04-12 19:39:01 -0300

    ajuste de diretorios na arquitetura do projeto

--- app/Http/Controllers/System/ProductController.php
+++ app/Http/Controllers/System/ProductController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\System;
 
+use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Helpers\SkuHelper;
 use Illuminate\Http\Request;
