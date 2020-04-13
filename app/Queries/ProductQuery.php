340adc97f23aec6bb713323d02c1c4de73a3ef7e
Igor Santos <igors.d@hotmail.com>
2020-04-13 00:35:57 -0300

    refatoracao e add novos metodos na classe ProductQuery

--- app/Queries/ProductQuery.php
+++ app/Queries/ProductQuery.php
@@ -7,26 +7,59 @@ use Illuminate\Database\Eloquent\Collection;
 
 final class ProductQuery
 {
-    public function countAllWithActive()
+    /**
+     * Busca todos produtos ativos
+     */
+    public function gettAllWithActive()
     {
         return Product::query()
         ->where('active', 1)
-        ->count();
+        ->get();
     }
 
-    public function countAllWithLowStock()
-    {
-        return Product::query()
-        ->where('active', 1)
-        ->where('amount', '<', 100)
-        ->count();
-    }
-
+    /**
+     * Busca os últimos cinco produtos cadastrados
+     */
     public function getLastFiveProducts()
     {
         return Product::query()
         ->orderBy('created_at', 'desc')
         ->take(5)
+        ->get();
+    }
+
+    /**
+     * Busca todos produtos com quantidade abaixo de 100
+     */
+    public function getAllWithLowStock()
+    {
+        return Product::query()
+        ->where('active', 1)
+        ->where('amount', '<', 100)
+        ->get();
+    }
+
+    /**
+     * Busca produtos cadastrados por data
+     */
+    public function getRegisteredByDateAndDay($column, $dateStart, $dateEnd)
+    {
+        return Product::query()
+        ->whereBetween($column, [$dateStart, $dateEnd])
+        ->orderBy($column, 'desc')
+        ->withTrashed()
+        ->get();
+    }
+
+    /**
+     * Busca produtos por tipo de entrada
+     */
+    public function getRegisterByInsertInAndDate($entry, $dateStart, $dateEnd)
+    {
+        return Product::query()
+        ->where('insert_in', $entry)
+        ->whereBetween('created_at', [$dateStart, $dateEnd])
+        ->withTrashed()
         ->get();
     }
 }
